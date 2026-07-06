1e8fc486dea4b2c8d78cbf41c9d4610a7d20cdcc
Sho Tamakawa <s.tamakawa@s-crew.jpn.com>
2026-07-06 16:53:49 +0900

    問題1-7追加

---
+++ src/Question1/Sub_Q7/Main.java
@@ -0,0 +1,14 @@
+package Question1.Sub_Q7;
+
+public class Main {
+    public Main() {
+    }
+
+    /* 解説：定数PIが細かい桁まで持つため、そのまま出力すると小数点以下が長々と表示されてしまう。 */
+    /* そのため、Stringのformatで%.3fを指定することで小数点以下3桁までの実数を出力することができる。 */
+    public void main() {
+        float x = 3.5f;
+
+        System.out.println( "半径 " + x + " の円の面積は" + String.format("%.3f",(x*x*Math.PI)) );
+    }
+}
